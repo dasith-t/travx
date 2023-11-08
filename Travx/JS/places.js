2829bcf7b5206f135f938625cacdbc49718ec712
Dasith Tharusha Kodithuwakku <dasiththarusha1@gmail.com>
2023-11-08 10:29:47 +0530

    Add files via upload

---
+++ Travx/JS/places.js
@@ -0,0 +1,310 @@
+// Data goes here
+const data = [
+  {
+    name: "Sigiriya",
+    description:
+      "Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka. The name refers to a site with historical and archaeological significance that is dominated by a massive column of rock around 180 metres high.",
+    imageWide:
+      "https://images.unsplash.com/photo-1612862862126-865765df2ded?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+    imageTall:
+      "https://images.unsplash.com/photo-1580877854178-95067799acfb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+    image1:
+      "https://images.unsplash.com/photo-1594391045445-64ea3c6ff16b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+    image2:
+      "https://images.unsplash.com/photo-1623262754278-73b95a39b9fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+    facts: [
+      "The citadel on top of Sigiriya was built by King Kasyapa who ruled from 477 to 495 CE. He choose this as his royal residence as he was worried of potential attacks from his brother Moggallana, the rightful heir to the throne. From atop of Sigiriya he had unobstructed views of the surrounding area.",
+      "The rock is 200 metres high and visitors can walk the 1,200 steps, divided amongst a number of staircases, to the top.",
+      "Prior to King Kasyapa using Sigiriya as his residence, it’s thought the rock was a Buddhist monastery. It was once again used as a monastery after Kasyapa’s death till it was abandoned in the 14th century.",
+      "The city includes water gardens, a moat, terraced gardens and archaeological features.",
+    ],
+  },
+  
+  {
+    name: "Sinharaja Forest Reserve",
+    description:
+      "Sinharaja Forest Reserve is a forest reserve and a biodiversity hotspot in Sri Lanka. It is a place with  international significance and with aBiosphere Reserve and World Heritage Site by UNESCO.",
+    imageWide:
+      "https://www.rainforest-ecolodge.com/wp-content/uploads/2015/07/Sinharaja-slider01.jpg",
+    imageTall:
+      "https://2abe47a55cfe3c2aba27-1868d38b8806f9b6ecce0a5b84ee61c5.ssl.cf1.rackcdn.com/sinharaja-rainforest-national-park-waterfall-landscape-nobody-sri-lanka.jpg",
+    image1:
+      "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_728/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/to5e3sjxoqnwywfrmbvw/SinharajaForestReservePrivateTourfromBentota.jpg",
+    image2:
+      "https://blueelephant.tours/wp-content/uploads/2022/08/Sinharaja-Rainforest-Day-Trek-from-Colombo.jpg",
+    facts: [
+      "There is much endemic wildlife, especially birds, but the reserve is also home to over 50% of Sri Lanka's endemic species of mammals and butterflies, as well as many kinds of insects, reptiles and rare amphibians.",
+      "The best time to visit Sinharaja Forest Reserve is between January to May and from August to December. ",
+      "The only way to enter Sinharaja is on foot, accompanied by a national guide.",
+      "The reserve can be entered from three different sides, but the more common entrance is via Deniyaya, a small and dusty town on the edge of the rain forest.",
+    ],
+  },
+
+
+  {
+    name: "Lotus Tower",
+    description:
+      "Lotus Tower (Sinhala: නෙලුම් කුලුන; Tamil: தாமரைக் கோபுரம்), also referred to as Colombo Lotus Tower is a tower 350 m (1,150 ft) high, located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. As of 2019, the tower is the tallest self-supported structure in South Asia; the second tallest structure in South Asia after the guy-wire-supported INS Kattabomman in India; the 11th tallest tower in Asia, and the 19th tallest tower in the world. It was first proposed to be built in the suburb of Peliyagoda, but later the Government of Sri Lanka decided to change the location. The lotus-shaped tower is used for communication, observation and other leisure facilities. Construction cost $104.3 million, funded by EXIM Bank of the People's Republic of China.",
+    imageWide:
+      "https://images.unsplash.com/photo-1546656495-fc838de15e5c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    imageTall:
+      "https://images.unsplash.com/photo-1567962266573-75f4640e800c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    image1:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lotus_Tower.jpg/250px-Lotus_Tower.jpg",
+    image2:
+      "https://th.thgim.com/news/international/scb0ma/article29434203.ece/ALTERNATES/FREE_960/SRILANKA-LOTUS-TOWER",
+    facts: [
+      "Sri Lanka on September 16 unveiled South Asia’s tallest tower, costing over $100 million, 80% of which has been funded by China under the controversial Belt and Road Initiative (BRI).",
+      "The 350-metre-tall 17-storey Lotus Tower, located in the heart of Colombo city, comprises a television tower, a hotel, a telecommunications museum, restaurants, auditorium, an observation deck, a shopping mall and a conference centre.",
+      "The tower, spread over an area of 30,600 sq. metres, was constructed at a cost of over $100 million, 80% of which was funded by China, Sri Lankan media reports said.",
+      "The tower, which is expected to function as Sri Lanka’s ‘digital TV tower’, is built with fibre optic cables and is fully equipped with necessary telecommunication equipment.",
+    ],
+  },
+
+  {
+    name: "Ella",
+    description:
+      "Ella is a small town in the Badulla District of Uva Province, Sri Lanka governed by an Urban Council. It is approximately 200 kilometres east of Colombo and is situated at an elevation of 1,041 metres above sea level. The area has a rich bio-diversity, dense with numerous varieties of flora and fauna.",
+    imageWide:
+      "https://images.unsplash.com/photo-1566296314736-6eaac1ca0cb9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80",
+    imageTall:
+      "https://images.unsplash.com/photo-1586008481877-7dd7c8236d00?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    image1:
+      "https://images.unsplash.com/photo-1588247094782-861f3edcb24d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
+    image2:
+      "https://images.unsplash.com/photo-1586496235784-d832d52d7bf5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+    facts: [
+      "Ella railway station is the 75th station on the Main Line and is located 271.03 km (168.41 mi) from Colombo. The station has one platform and all trains running on the Main Line stop at the station. The station opened in July 1918.",
+      "Dhowa temple, a 2,000-year-old rock temple, is located on the Badulla-Bandarawela Road. It contains a 12 metres (39 ft) unfinished Buddha statue carved into the surrounding rock.",
+      "There is Little Adam's Peak, a 1,141 m (3,743 ft) pyramidal-shaped hill, located to the south-east of the town. Named after the larger Adam's Peak.",
+      "Ravana Ella Falls, a 25 m (82 ft) waterfall, is located approximately 6 km (4 mi) away from the town",
+    ],
+  },
+  
+   {
+    name: "Kataragam",
+    description:
+      "Kataragama is a pilgrimage town sacred to Hindu, Buddhist and indigenous Vedda people of Sri Lanka located in the sourthern part of the country. It is well known for the “Kiri Vehera” Pagoda and for the temple dedicated to the god “Kataragama”which are worshiped by many.",
+    imageWide:
+      "https://www.trawell.in/admin/images/upload/189427232Kataragama.jpg",
+    imageTall:
+      "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2019/01/Places-to-Visit-in-Kataragama-og.jpg",
+    image1:
+      "https://live.staticflickr.com/65535/49683576017_6a31e97224_b.jpg",
+    image2:
+      "https://images.unsplash.com/photo-1586496235784-d832d52d7bf5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+    facts: [
+      "Kataragama is a fast developing township surrounded by jungle, and in ancient times was the seat of Government for many Sinhalese kings. The pilgrimage town is also an important tourist destination due to its closeness to Yala National park.",
+      "The ancient Kiri Vehera Buddhist stupa, which is believed to be built by the regional king Mahasena in the 6th century BC is worshiped by many Buddhist then followed by the Maha Kovil dedicated to god “Kataragama”. ",
+      "Although Kataragama was a small village in medieval times, today it is a fast-developing township surrounded by jungle in the southeastern region of Sri Lanka.",
+      " A major annual festival accompanied by a Perahera is held in honor of the god during the months of July and August where a large crowds come to see it.",
+    ],
+  },
+
+
+  {
+    name: "Polonnaruwa",
+    description:
+      "Polonnaruwa is a major city in Sri Lanka dating back before 1070AD. It is a city in North Central Province, Sri Lanka and the capital of Polonnaruwa District. Polonnaruwa is one of the ancient capitals of Sri Lanka, famous for its well-preserved ruins of an ancient Sinhala civilization.",
+    imageWide:
+      "https://i.pinimg.com/564x/26/23/d9/2623d91512e675c972291bafc2b7ba65.jpg",
+    imageTall:
+      "https://i.pinimg.com/564x/95/83/8a/95838a54b047d84b2a7e79e2c1febe76.jpg",
+    image1:
+      "https://i.pinimg.com/564x/1c/b1/6b/1cb16bb4b8fe397cf7ee894d692f39b8.jpg",
+    image2:
+      "https://i.pinimg.com/564x/3f/c7/72/3fc77240cb0ef0333c19a4543fbdec3d.jpg",
+    facts: [
+      "Polonnaruwa is famous for its marvellous irrigation systems which were mainly built during the ruling period of King Parakramabahu I, especially the irrigation system called the Parakrama Samudraya.",
+      "The Parakrama Samudraya resovoir which was built by expanding the Eramudu Wewa and the Dambutulu Wewa and has an area of around 22.6 Square Kilometers.",
+      "Polonnaruwa was the Second Kingdom in Sri Lanka which was ruled by many great rulers such as King Parakramabahu, King Viayabahu, King Nissankamalla etc.",
+      "Polonnaruwa is a UNESCO World Heritage Site and sacred to Sri Lankans.",
+    ],
+  },
+  {
+    name: "Anuradhapura",
+    description:
+      "Anuradhapura is a major city located in north central plain of Sri Lanka. It is the capital city of North Central Province and the capital of Anuradhapura District. The city lies 205 kilometers north of the current capital of Colombo in the North Central Province, on the banks of the historic Malwathu Oya.",
+    imageWide:
+      "https://images.unsplash.com/photo-1546656495-fc838de15e5c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    imageTall:
+      "https://images.unsplash.com/photo-1567962266573-75f4640e800c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    image1:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lotus_Tower.jpg/250px-Lotus_Tower.jpg",
+    image2:
+      "https://th.thgim.com/news/international/scb0ma/article29434203.ece/ALTERNATES/FREE_960/SRILANKA-LOTUS-TOWER",
+    facts: [
+      "1. It was the first capital city of Sri Lanka",
+      " After this, it was abandoned for nearly one thousand years",
+      "It is home to what is thought to be the world’s oldest tree",
+      "It's home to the most complex irrigation systems discovered in the ancient world",
+    ],
+  },
+
+  {
+    name: "Kandy",
+    description:
+      "Kandy is a large city in central Sri Lanka. It's set on a plateau surrounded by mountains, which are home to tea plantations and biodiverse rainforest.",
+    imageWide:
+      "https://www.archaeology.lk/wp-content/uploads/2020/11/temple-sacred-tooth-relic-kandy-sri-lanka.jpg",
+    imageTall:
+      "https://m.psecn.photoshelter.com/img-get2/I0000TCv5DqMftXo/fit=1000x750/Kandy-the-island-in-Kandy-Lake-at-sunrise-Kandy-Central-Province-Sri-Lanka-Asia.jpg",
+    image1:
+      "https://st.depositphotos.com/1609126/4900/i/600/depositphotos_49003045-stock-photo-temple-of-the-tooth-kandy.jpg",
+    image2:
+      "https://www.kandyescapes.com/wp-content/uploads/2020/04/lake-mob.jpg",
+    facts: [
+      "Kandy is the second largest city in Sri Lanka and the capital of the Central Province.",	
+      "Kandy is the home of the Temple of the Tooth Relic, which houses one of the Buddha's teeth.",
+      "Kandy is the last capital of the ancient kings' era of Sri Lanka. The city was the last capital of the Sinhalese Kingdom until 1815, when the island was ceded to the British Empire.",
+      "Kandy is a UNESCO World Heritage Site.",
+    ],
+  },
+
+   
+];
+
+const mobileNav = document.querySelector(".links");
+const hamburger = document.querySelector(".hamburger");
+const button = document.querySelectorAll(".moreInfo");
+const model = document.querySelector(".model");
+const modelContent = document.querySelector(".modelContent");
+const closeModel = document.querySelector(".closeButton");
+const title = document.querySelector(".modelContent h1");
+const imageContainer = document.querySelector(".images");
+const factTitle = document.querySelector(".factTitle");
+const navbar = document.querySelector("nav");
+const call = document.querySelector(".call");
+const discord = document.querySelector(".discord");
+
+const details = document.querySelector(".details");
+
+const factContainer = document.querySelector(".factContainer");
+let place;
+
+call.addEventListener("click", () => {
+  Swal.fire({
+    imageUrl: 'https://i.ibb.co/d04JBmC/image.png',
+    imageWidth: 75,
+    imageHeight: 75,
+    html:  `My phone number is <b><a href="tel:+94711436311">071-1436311</a></b>`,
+  })
+});
+
+discord.addEventListener("click", () => {
+  Swal.fire({
+    imageUrl: 'https://i.ibb.co/JsdzLhm/image-2021-10-08-173444.png',
+    imageWidth: 75,
+    imageHeight: 75,
+    html:  `My Discord Handle is <b>chamindu_js#6588</b>`,
+  })
+});
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loaderr");
+  loader.style.transform = "translateY(-100%)";
+});
+
+const renderPlace = (item) => {
+  factTitle.innerHTML += " " + place;
+  console.log(factTitle);
+  // Render the details in the container
+  let placeName = document.createElement("h1");
+  let placeDescription = document.createElement("p");
+  let imageTall = document.createElement("div");
+  let imageWide = document.createElement("div");
+  let image1 = document.createElement("div");
+  let image2 = document.createElement("div");
+
+  for (let i = 0; i < 4; i++) {
+    let fact = document.createElement("li");
+    fact.textContent = item.facts[i];
+    factContainer.appendChild(fact);
+  }
+
+  placeName.setAttribute("class", "placeTitle");
+  placeDescription.setAttribute("class", "placeDescription");
+
+  imageTall.setAttribute("class", "image tall");
+  imageTall.setAttribute(
+    "style",
+    "background-image: url(" + item.imageTall + ")"
+  );
+
+  imageWide.setAttribute("class", "image wide");
+  imageWide.setAttribute(
+    "style",
+    "background-image: url(" + item.imageWide + ")"
+  );
+
+  image1.setAttribute("class", "image");
+  image1.setAttribute("style", "background-image: url(" + item.image1 + ")");
+
+  image2.setAttribute("class", "image");
+  image2.setAttribute("style", "background-image: url(" + item.image2 + ")");
+
+  placeName.innerText = item.name;
+  placeDescription.innerText = item.description;
+
+  details.appendChild(placeName);
+  details.appendChild(placeDescription);
+  imageContainer.appendChild(imageTall);
+  imageContainer.appendChild(imageWide);
+  imageContainer.appendChild(image1);
+  imageContainer.appendChild(image2);
+  imageContainer.appendChild(image2);
+};
+
+button.forEach((link) => {
+  link.addEventListener("click", function () {
+    modelContent.classList.add("active");
+    model.classList.add("active");
+
+    place = link.getAttribute("data");
+
+    data.filter(function (item) {
+      if (item.name === place) {
+        renderPlace(item);
+      }
+    });
+  });
+});
+
+//Event listeners
+
+hamburger.addEventListener("click", function () {
+  mobileNav.classList.toggle("active");
+  hamburger.classList.toggle("is-active");
+  details.innerHTML = "";
+  imageContainer.innerHTML = "";
+  factContainer.innerHTML = "";
+});
+
+model.addEventListener("click", function () {
+  model.classList.remove("active");
+  modelContent.classList.remove("active");
+
+  details.innerHTML = "";
+  imageContainer.innerHTML = "";
+  factContainer.innerHTML = "";
+  factTitle.innerHTML =
+    'Here are some <span class="factHighlight">fun facts</span> about';
+});
+
+closeModel.addEventListener("click", function () {
+  model.classList.remove("active");
+  modelContent.classList.remove("active");
+  details.innerHTML = "";
+  imageContainer.innerHTML = "";
+  factContainer.innerHTML = "";
+  factTitle.innerHTML =
+    'Here are some <span class="factHighlight">fun facts</span> about';
+});
+
+window.onscroll = function () {
+  if (scrollY > 50) {
+    navbar.classList.add("active");
+  } else {
+    navbar.classList.remove("active");
+  }
+};
